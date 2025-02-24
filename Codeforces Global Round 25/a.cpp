c96edffc0e67a5ab105122c60fd589ef944dfd82
kpeel <jaeyeon.kim@seavantage.com>
2025-02-25 01:23:39 +0900

    solve a problem in Educational Codeforces Round 174

--- Codeforces Global Round 25/a.cpp
+++ Codeforces Global Round 25/a.cpp
@@ -45,11 +45,16 @@ const int inf = 2e9;
 const int size = 1 << 18;
 const int mod = 1e9 + 7;
 
-namespace std {
-  template <> struct hash<std::vector<int>> {
-    size_t operator()(const std::vector<int> &v) const {
+namespace std
+{
+  template <>
+  struct hash<std::vector<int>>
+  {
+    size_t operator()(const std::vector<int> &v) const
+    {
       size_t hash_value = 0;
-      for (int i : v) {
+      for (int i : v)
+      {
         hash_value ^= std::hash<int>()(i);
       }
       return hash_value;
@@ -57,50 +62,62 @@ namespace std {
   };
 }
 
-const ll MOD=1e7;
+const ll MOD = 1e7;
 vl fact = {1};
-ll pow_mod(ll x, ll p) {
-  if (p == 0) {
+ll pow_mod(ll x, ll p)
+{
+  if (p == 0)
+  {
     return 1;
   }
-  if (p % 2 == 0) {
+  if (p % 2 == 0)
+  {
     ll y = pow_mod(x, p / 2);
     return (y * y) % MOD;
   }
   return (x * pow_mod(x, p - 1)) % MOD;
 }
-ll inv(ll x) {
+ll inv(ll x)
+{
   return pow_mod(x, MOD - 2);
 }
-ll cnk(ll n, ll k) {
+ll cnk(ll n, ll k)
+{
   ll res = fact[n];
   res = (res * inv(fact[k])) % MOD;
   res = (res * inv(fact[n - k])) % MOD;
   return res;
 }
 
-void solve() {
-  int n;string s;cin>>n>>s;
-  int cnt=0;
-  int mi=inf;
-  int mx=-1;
-  for(int i=0;i<n;i++){
-    if(s.at(i)=='1'){
+void solve()
+{
+  int n;
+  string s;
+  cin >> n >> s;
+  int cnt = 0;
+  int mi = inf;
+  int mx = -1;
+  for (int i = 0; i < n; i++)
+  {
+    if (s.at(i) == '1')
+    {
       cnt++;
-      mi=min(mi,i);
-      mx=max(mx,i);
+      mi = min(mi, i);
+      mx = max(mx, i);
     }
   }
-  cout<<((cnt%2==1)||(cnt==2&&mx-mi==1)?"NO\n":"YES\n");
+  cout << ((cnt % 2 == 1) || (cnt == 2 && mx - mi == 1) ? "NO\n" : "YES\n");
 }
 
-int main(void) {
+int main(void)
+{
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   freopen("a.input.txt", "r", stdin);
   int T;
   cin >> T;
-  while (T-- > 0) {
+  while (T-- > 0)
+  {
     solve();
   }
   return 0;
--- dir_creator.sh
+++ dir_creator.sh
@@ -1,10 +1,90 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
 read -p "대회 명을 입력하세요: " contestName
 read -p "문제 개수를 입력하세요: " problemCount
 
-mkdir "${contestName}"
+# Create main directory
+if ! mkdir "${contestName}" 2>/dev/null; then
+    echo "Error: Directory ${contestName} already exists or cannot be created"
+    exit 1
+fi
 echo "${contestName} 디렉토리가 생성되었습니다."
+
+# Create .vscode directory
+mkdir "${contestName}/.vscode"
+echo ".vscode 디렉토리가 생성되었습니다."
+
+# Create c_cpp_properties.json
+cat > "${contestName}/.vscode/c_cpp_properties.json" << 'EOL'
+{
+    "configurations": [
+        {
+            "name": "Mac",
+            "includePath": [
+                "${workspaceFolder}/**"
+            ],
+            "defines": [],
+            "macFrameworkPath": [
+                "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks"
+            ],
+            "compilerPath": "/usr/bin/g++",
+            "cStandard": "c17",
+            "cppStandard": "c++17",
+            "intelliSenseMode": "macos-clang-arm64"
+        }
+    ],
+    "version": 4
+}
+EOL
+echo "c_cpp_properties.json 파일이 생성되었습니다."
+
+# Create tasks.json
+cat > "${contestName}/.vscode/tasks.json" << 'EOL'
+{
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "type": "shell",
+            "label": "C/C++: g++ build active file",
+            "command": "/usr/bin/g++",
+            "args": [
+                "-std=c++17",
+                "-stdlib=libc++",
+                "-g",
+                "${file}",
+                "-o",
+                "${fileDirname}/${fileBasenameNoExtension}.out",
+                "&&",
+                "${fileDirname}/${fileBasenameNoExtension}.out"
+            ],
+            "options": {
+                "cwd": "${workspaceFolder}"
+            },
+            "problemMatcher": [
+                "$gcc"
+            ],
+            "group": {
+                "kind": "build",
+                "isDefault": true
+            }
+        },
+        {
+            "label": "exec",
+            "type": "shell",
+            "command": "${fileDirname}/${fileBasenameNoExtension}.out",
+            "group": {
+                "kind": "build",
+                "isDefault": true
+            }
+        }
+    ]
+}
+EOL
+echo "tasks.json 파일이 생성되었습니다."
+
 i=1
 
 while [ $i -le $problemCount ]
